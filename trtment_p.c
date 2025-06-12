d7623eb2904722885f3c58e0dfdd67d76b8b246d
JulesChr <jules.chevrier@isen-ouest.yncrea.fr>
2025-06-12 15:43:04 +0200

    traitement p 2

--- trtment_p.c
+++ trtment_p.c
@@ -4,6 +4,26 @@
 #include <stdbool.h>
 
 
+
+void lire_p(const char* filename){
+    FILE* file = fopen(filename, "r");
+    if(!file){
+        perror("error while file's opening");
+        return;
+    }
+    
+    char line[512];
+    
+    while(fgets(line, sizeof(line), file)){    // ...
+        if(strstr("<p>",line)){ // verifie si la ligne contient la balise p
+            char texte[512];
+            sscanf(line,"<chapter id\"%d\">%[^>]s",texte); // extrait l'id et le titre de la balise chapter
+        }
+    }
+
+}
+
+
 // Affiche le contenu dans une balise <p>
 void afficher_p(const char* txt) {
     printf("<p>");
