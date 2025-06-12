45de830a38ee63a88ccaebce7156cfb905d54eaa
Asma BEN CHAABANE <asma.ben-chaabane@iesn-ouest.yncrea.fr>
2025-06-12 10:36:31 +0200

    sdfg

---
+++ main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#define LINE_SIZE 512
+
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        fprintf(stderr, "Usage: %s <filename>\n", argv[0]);
+        return EXIT_FAILURE;
+    }
+
+    const char *filename = argv[1];
+    FILE *file = fopen(filename, "r");
+    if (!file) {
+        perror("Failed to read file");
+        return EXIT_FAILURE;
+    }
+
+    //fprintf(file, "This is a new file created by the create program.\n");
+    char line[LINE_SIZE];
+    if(fgets(line, sizeof(line), file)) {
+        printf("Reading the line %s", line);
+    }
+    fclose(file);
+    return EXIT_SUCCESS;
+}
