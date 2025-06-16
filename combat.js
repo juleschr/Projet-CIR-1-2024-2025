e93d87ea6e56a6dd6ee90b19bb6b2dfdf5b9e2b6
juleschr <jules.chevrier@isen-ouest.yncrea.fr>
2025-06-16 23:29:46 +0200

    defense

--- combat.js
+++ combat.js
@@ -4,8 +4,8 @@ function attaque(vie, atk, CD){
     }
     
     let lancer_D20 = (Math.random()*20)+1; // donne un nombre entre 1 et 20 
-    let degats = lancer_D20+ atk_perso;
-    if(degats >= CD){
+    let attaque_reussie = lancer_D20+ atk_perso;
+    if(attaque_reussie >= CD){
         let lancer_D8 = (Math.random()*8)+1;
         let degats_infliger = (lancer_D8*2) - CD + atk;
         vie = vie - degats_infliger;
@@ -14,7 +14,18 @@ function attaque(vie, atk, CD){
 }
 
 function defense(vie , def , CD){
-    
+    if (vie == 0) {
+        return 0;
+    }
+
+    let lancer_D20 = (Math.random() * 20) + 1; // nombre entre 1 et 20
+    let defense_reussie = lancer_D20 + def;
+    if (defense_reussie >= CD) {
+        let lancer_D6 = (Math.random() * 6) + 1;
+        let vie_recuperee = (lancer_D6 * 2) - CD + def;
+        vie = vie + vie_recuperee;
+    }
+    return defense(vie, def, CD);
 }
 
 function main(){
